099bc24b224e59f600941d5137abf68e5757c732
Aadeesh11 <asoswal@gmail.com>
2022-01-23 22:45:04 +0530

    Graphql code

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require("./graphql/schema");
+
 const { connectDB } = require("./db");
 const app = express();
 dotenv.config();
 connectDB();
+
 app.get("/", (req, res) => {
-  res.json({ msg: "hello world" });
+  res.json({ msg: "hello world! go to /graphql" });
 });
+
+app.use("/graphql", 
+  graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
 
 app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}/`);
